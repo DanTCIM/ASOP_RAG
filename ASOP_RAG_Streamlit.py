2005f88d0fa82335297090d5d1a840d328a2a146
Dan Kim <dan153@dans-mbp.lan>
2024-02-20 22:43:52 -0600

    update streamlit design

--- ASOP_RAG_Streamlit.py
+++ ASOP_RAG_Streamlit.py
@@ -26,13 +26,18 @@ with st.sidebar:
     st.write("Powered by OpenAI's GPT 3.5-Turbo: Harness the capabilities of LLM to search for and retrieve information on ASOP.")
     
     link1 = "http://www.actuarialstandardsboard.org/wp-content/uploads/2023/12/ASOPs-as-of-Decemeber-2023.zip"
-    st.write(f"ASOP documents are downloaded from Actuarial Standards Board's [link]({link1}) as of December 2023.")
+    st.caption(f"ASOP documents are downloaded from Actuarial Standards Board's [link]({link1}) as of December 2023.")
     
-    st.subheader('⚙️ RAG Parameters')
-    num_source = st.sidebar.slider('No. of sources to return', min_value=4, max_value=20, value=5, step=1)
-    _lambda_mult = st.sidebar.slider('Source precision', min_value=0.0, max_value=1.0, value=0.5, step=0.25)
-    st.write("The search algorithm fetches 20 sources regardless of the number of sources to return.")
-    st.write("Source precision controls how fetched sources are diverse. 0 being the most diverse, 1 being the least diverse. The diversity helps LLM generate a response considering multiple aspects.")
+    def clear_chat_history():
+        st.session_state.messages = [{"role": "ai", "content": "What is your question on ASOP?", "type": "text"}]
+    st.button('Clear Chat History', on_click=clear_chat_history)
+   
+    with st.container(border=True):
+        st.subheader('⚙️ RAG Parameters')
+        num_source = st.slider('Top N sources to view:', min_value=4, max_value=20, value=5, step=1)
+        _lambda_mult = st.slider('Source diversity (lambda):', min_value=0.0, max_value=1.0, value=0.5, step=0.25)
+        with st.expander("What is diversity?"):
+            st.caption("Maximal Marginal Relevance (MMR) tries to reduce redundancy of fetched documents and increase diversity. 0 being the most diverse, 1 being the least diverse. 0.5 is a balanced state.")
 
     st.subheader('📖 Further Notes')
     st.write("Responses are based on LLM's features and search algorithms, and should not be relied upon as definitive or error-free. Users are encouraged to review the source contexts carefully. The sources may appear less relevant to the question due to the diversity of the search.")
@@ -45,8 +50,6 @@ with st.sidebar:
 # # Set up the title and input
 st.header("Actuarial Standards of Practice (ASOP) Q&A Machine using Retrieval Augmented Generation (RAG)")
 st.write("Please see sidebar for further information.")
-# Prompt the user for a question on ASOP
-usr_input = st.text_input("What is your question on ASOP?: ")
 
 # # Model and directory setup
 embeddings_model = OpenAIEmbeddings()
@@ -90,15 +93,16 @@ rag_chain_with_source = RunnableParallel(
 
 
 # # Generate output
-def generate_output():
+def generate_output(prompt_input):
     # Invoke the RAG chain with the user input as the question
-    output = rag_chain_with_source.invoke(usr_input)
+    output = rag_chain_with_source.invoke(prompt_input)
 
     # Generate the Markdown output with the question, answer, and context
-    markdown_output = "### Question\n{}\n\n### Search summarized by GPT 3.5-turbo\n{}\n\n### Sources\n".format(output['question'], output['answer'])
+    markdown_output = "{}".format(output['answer'])
 
     last_page_content = None  # Variable to store the last page content
     i = 1 # Source indicator
+    markdown_source_output = ''
 
     # Iterate over the context documents to format and include them in the output
     for doc in output['context']:
@@ -106,14 +110,42 @@ def generate_output():
         
         # Check if the current content is different from the last one
         if current_page_content != last_page_content:
-            markdown_output += "- **Source {}**: {}, page {}:\n\n{}\n".format(i, doc.metadata['source'], doc.metadata['page'], current_page_content)
+            markdown_source_output += "#### Source {}: {}, page {}\n\n{}\n".format(i, doc.metadata['source'], doc.metadata['page'], current_page_content)
             i = i + 1
         last_page_content = current_page_content  # Update the last page content
     
-    # Display the Markdown output
-    st.markdown(markdown_output)
+    # Display the output for markdown
+    return markdown_output, markdown_source_output
 
-# Let's search ASOP!
-if st.button('Search ASOP'):
-    with st.spinner('Retrieving info and generating response...'):
-        generate_output()
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "ai", "content": "What is your question on ASOP?", "type": "text"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    if message['type'] == 'source':
+        with st.expander("See sources"):
+            st.write(message["content"])    
+    else:
+        with st.chat_message(message["role"]):
+            if message['type'] == 'text':
+                st.write(message["content"])
+
+# User-provided prompt
+if user_prompt := st.chat_input("What is your question on ASOP?"):
+    st.session_state.messages.append({"role": "user", "content": user_prompt, "type": "text"})
+    with st.chat_message("user"):
+        st.write(user_prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "ai":
+    with st.chat_message("ai"):
+        with st.spinner("Retrieving info and generating response..."):
+            response, sources = generate_output(user_prompt)
+            st.write(response)
+    with st.expander("See sources", expanded=True):
+        st.write(sources)
+    message = {"role": "ai", "content": response, "type": "text"}
+    source_expand = {"role": "ai", "content": sources, "type": "source"}
+    st.session_state.messages.append(message)
+    st.session_state.messages.append(source_expand)
