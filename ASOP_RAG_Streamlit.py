97788258914ae0b671f17437768d0ea53d57f78e
Dan Kim <Dan153TCIM@gmail.com>
2024-02-28 20:12:22 -0600

    Added comments

--- ASOP_RAG_Streamlit.py
+++ ASOP_RAG_Streamlit.py
@@ -6,102 +6,144 @@ import os
 # sqlite3 related (for Streamlit)
 import pysqlite3
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 # Langchain and Vector DB
 from langchain import hub
 from langchain_community.vectorstores import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from langchain_core.runnables import RunnableParallel # for RAG with source
+from langchain_core.runnables import RunnableParallel  # for RAG with source
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 import chromadb
 from langchain_core.prompts import ChatPromptTemplate
 import pandas as pd
 
-## API key setup 
+## API key setup
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 # # Set up the title and input
-st.header("Actuarial Standards of Practice (ASOP) Q&A Machine using Retrieval Augmented Generation (RAG)")
+st.header(
+    "Actuarial Standards of Practice (ASOP) Q&A Machine using Retrieval Augmented Generation (RAG)"
+)
 st.write("Please see sidebar for further information.")
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "ai", "content": "What is your question on ASOP?", "type": "text"}]
+    st.session_state.messages = [
+        {"role": "ai", "content": "What is your question on ASOP?", "type": "text"}
+    ]
 
 ## Sidebar
 with st.sidebar:
     st.title("ASOP Q&A Machine")
-    st.write("Powered by OpenAI's GPT 3.5-Turbo: Harness the capabilities of LLM to search for and retrieve information on ASOP.")
-    
+    st.write("**Built for educational purposes only.**")
+    st.write(
+        "Powered by OpenAI's GPT 3.5-Turbo: Harness the capabilities of LLM to search for and retrieve information on ASOP."
+    )
+
     link1 = "http://www.actuarialstandardsboard.org/wp-content/uploads/2023/12/ASOPs-as-of-Decemeber-2023.zip"
-    st.caption(f"ASOP documents are downloaded from Actuarial Standards Board's [link]({link1}) as of December 2023.")
+    st.caption(
+        f"ASOP documents are downloaded from Actuarial Standards Board's [link]({link1}) as of December 2023."
+    )
 
     col1, col2 = st.columns(2)
     with col1:
+
         def convert_df():
-            df = pd.DataFrame(st.session_state.messages)        
-            return df.to_csv().encode('utf-8')
+            df = pd.DataFrame(st.session_state.messages)
+            return df.to_csv().encode("utf-8")
+
         st.download_button(
             label="Download Chat",
             help="Download chat history in CSV",
             data=convert_df(),
-            file_name='chat_history.csv',
-            mime='text/csv',
+            file_name="chat_history.csv",
+            mime="text/csv",
             use_container_width=True,
         )
     with col2:
+
         def clear_chat_history():
-            st.session_state.messages = [{"role": "ai", "content": "What is your question on ASOP?", "type": "text"}]
+            st.session_state.messages = [
+                {
+                    "role": "ai",
+                    "content": "What is your question on ASOP?",
+                    "type": "text",
+                }
+            ]
+
         st.button(
-            'Clear Chat',
+            "Clear Chat",
             help="Clear chat history",
             on_click=clear_chat_history,
             use_container_width=True,
         )
 
     with st.container(border=True):
-        st.subheader('⚙️ RAG Parameters')
-        num_source = st.slider('Top N sources to view:', min_value=4, max_value=20, value=5, step=1)
-        flag_mmr = st.toggle('Diversity search',
-                            help="Diversity search, i.e., Maximal Marginal Relevance (MMR) tries to reduce redundancy of fetched documents and increase diversity. 0 being the most diverse, 1 being the least diverse. 0.5 is a balanced state.")
-        _lambda_mult = st.slider('Diversity parameter (lambda):', min_value=0.0, max_value=1.0, value=0.5, step=0.25)
-        
-        #with st.expander("What is diversity?"):
+        st.subheader("⚙️ RAG Parameters")
+        num_source = st.slider(
+            "Top N sources to view:", min_value=4, max_value=20, value=5, step=1
+        )
+        flag_mmr = st.toggle(
+            "Diversity search",
+            help="Diversity search, i.e., Maximal Marginal Relevance (MMR) tries to reduce redundancy of fetched documents and increase diversity. 0 being the most diverse, 1 being the least diverse. 0.5 is a balanced state.",
+        )
+        _lambda_mult = st.slider(
+            "Diversity parameter (lambda):",
+            min_value=0.0,
+            max_value=1.0,
+            value=0.5,
+            step=0.25,
+        )
+
+        # with st.expander("What is diversity?"):
         #    st.caption("Maximal Marginal Relevance (MMR) tries to reduce redundancy of fetched documents and increase diversity. 0 being the most diverse, 1 being the least diverse. 0.5 is a balanced state.")
 
-    st.subheader('📖 Further Notes')
-    st.write("Responses are based on LLM's features and search algorithms, and should not be relied upon as definitive or error-free. Users are encouraged to review the source contexts carefully. The sources may appear less relevant to the question due to the diversity of the search.")
+    st.subheader("📖 Further Notes")
+    st.write(
+        "Responses are based on LLM's features and search algorithms, and should not be relied upon as definitive or error-free. Users are encouraged to review the source contexts carefully. The sources may appear less relevant to the question due to the diversity of the search."
+    )
     link2 = "https://www.actuarialstandardsboard.org/standards-of-practice/"
-    st.write(f"Please visit [Actuarial Standard Board's ASOP site]({link2}) to get the latest ASOP.")
-    
+    st.write(
+        f"Please visit [Actuarial Standard Board's ASOP site]({link2}) to get the latest ASOP."
+    )
+
     link3 = "https://github.com/DanTCIM/ASOP_RAG"
-    st.write(f"The Python codes and documentation of the project are in [GitHub]({link3}).")
+    st.write(
+        f"The Python codes and documentation of the project are in [GitHub]({link3})."
+    )
 
 # # Model and directory setup
 embeddings_model = OpenAIEmbeddings()
 db_directory = "./data/chroma_db1"
-llm = ChatOpenAI(model_name="gpt-3.5-turbo-0125", 
-                 temperature=0) # context window size 16k for GPT 3.5 Turbo
+llm = ChatOpenAI(
+    model_name="gpt-3.5-turbo-0125", temperature=0
+)  # context window size 16k for GPT 3.5 Turbo
 
 # # Get a Chroma vector database with specified parameters
-vectorstore = Chroma(embedding_function=embeddings_model, persist_directory=db_directory)
+vectorstore = Chroma(
+    embedding_function=embeddings_model, persist_directory=db_directory
+)
 
 # # Retrieve and RAG chain
 # Create a retriever using the vector database as the search source
 if flag_mmr:
-    retriever = vectorstore.as_retriever(search_type="mmr", 
-                                         search_kwargs={'k': num_source, 'lambda_mult': _lambda_mult}) 
-    # Use MMR (Maximum Marginal Relevance) to find a set of documents 
+    retriever = vectorstore.as_retriever(
+        search_type="mmr", search_kwargs={"k": num_source, "lambda_mult": _lambda_mult}
+    )
+    # Use MMR (Maximum Marginal Relevance) to find a set of documents
     # that are both similar to the input query and diverse among themselves
-    # Increase the number of documents to get, and increase diversity 
+    # Increase the number of documents to get, and increase diversity
     # (lambda mult 0.5 being default, 0 being the most diverse, 1 being the least)
 else:
-    retriever = vectorstore.as_retriever(search_kwargs={'k': num_source}) # use similarity
+    retriever = vectorstore.as_retriever(
+        search_kwargs={"k": num_source}
+    )  # use similarity
 
 # Load the RAG (Retrieval-Augmented Generation) prompt
-#prompt_concise = hub.pull("rlm/rag-prompt")
+# prompt_concise = hub.pull("rlm/rag-prompt")
 
 qa_system_prompt = """You are a helpful assistant to help actuaries with question-answering tasks. \
 Use the following pieces of retrieved context to answer the question. \
@@ -118,17 +160,23 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+
 # Define a function to format the documents with their sources and pages
 def format_docs_with_sources(docs):
     formatted_docs = "\n\n".join(doc.page_content for doc in docs)
-    sources_pages = "\n".join(f"{doc.metadata['source']} (Page {doc.metadata['page'] + 1})" for doc in docs)
+    sources_pages = "\n".join(
+        f"{doc.metadata['source']} (Page {doc.metadata['page'] + 1})" for doc in docs
+    )
     # Added 1 to the page number assuming 'page' starts at 0 and we want to present it in a user-friendly way
 
     return f"Documents:\n{formatted_docs}\n\nSources and Pages:\n{sources_pages}"
 
+
 # Create a RAG chain using the formatted documents as the context
 rag_chain_from_docs = (
-    RunnablePassthrough.assign(context=(lambda x: format_docs_with_sources(x["context"])))
+    RunnablePassthrough.assign(
+        context=(lambda x: format_docs_with_sources(x["context"]))
+    )
     | prompt
     | llm
     | StrOutputParser()
@@ -146,42 +194,54 @@ def generate_output(prompt_input):
     output = rag_chain_with_source.invoke(prompt_input)
 
     # Generate the Markdown output with the question, answer, and context
-    markdown_output = "{}".format(output['answer'])
+    markdown_output = "{}".format(output["answer"])
 
     last_page_content = None  # Variable to store the last page content
-    i = 1 # Source indicator
-    markdown_source_output = ''
+    i = 1  # Source indicator
+    markdown_source_output = ""
 
     # Iterate over the context documents to format and include them in the output
-    for doc in output['context']:
-        current_page_content = doc.page_content.replace('\n', '  \n')  # Get the current page content
-        
+    for doc in output["context"]:
+        current_page_content = doc.page_content.replace(
+            "\n", "  \n"
+        )  # Get the current page content
+
         # Check if the current content is different from the last one
         if current_page_content != last_page_content:
-            markdown_source_output += "#### Source {}: {}, page {}\n\n{}\n".format(i, doc.metadata['source'].split("/data/ASOP/")[-1], doc.metadata['page'], current_page_content)
+            markdown_source_output += "#### Source {}: {}, page {}\n\n{}\n".format(
+                i,
+                doc.metadata["source"].split("/data/ASOP/")[-1],
+                doc.metadata["page"],
+                current_page_content,
+            )
             i = i + 1
         last_page_content = current_page_content  # Update the last page content
-    
+
     # Display the output for markdown
     return markdown_output, markdown_source_output
 
+
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "ai", "content": "What is your question on ASOP?", "type": "text"}]
+    st.session_state.messages = [
+        {"role": "ai", "content": "What is your question on ASOP?", "type": "text"}
+    ]
 
 # Display or clear chat messages
 for message in st.session_state.messages:
-    if message['type'] == 'source':
+    if message["type"] == "source":
         with st.expander("Review the top N sources"):
-            st.write(message["content"])    
+            st.write(message["content"])
     else:
         with st.chat_message(message["role"]):
-            if message['type'] == 'text':
+            if message["type"] == "text":
                 st.write(message["content"])
 
 # User-provided prompt
 if user_prompt := st.chat_input("What is your question on ASOP?"):
-    st.session_state.messages.append({"role": "user", "content": user_prompt, "type": "text"})
+    st.session_state.messages.append(
+        {"role": "user", "content": user_prompt, "type": "text"}
+    )
     with st.chat_message("user"):
         st.write(user_prompt)
 
